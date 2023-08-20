f2255ccfcc2e8f209b54fdf770e36b43d06cd056
richard <richard.galdeano3@gmail.com>
2023-08-20 15:45:17 +0200

    All blocks in place

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ FramePerSec = pygame.time.Clock()
 SCREEN_SIDE = 1000
 
 BLUE = (0, 0, 255)
+RED = (255, 0, 0)
 RECT_SIZE = (100, 50)
 
 DISPLAY_SURFACE = pygame.display.set_mode((SCREEN_SIDE, SCREEN_SIDE))
@@ -22,18 +23,22 @@ pygame.display.set_caption("Casse-Briques")
 
 
 
-# class Brick(pygame.sprite.Sprite):
-#
-#     def __init__(self, x, y, hits):
-#         super().__init__()
-#         self.size = (100, 50)
-#         self.hits = hits
-#         self.x = x
-#         self.y = y
-#         self.color = BLUE
+class Brick(pygame.sprite.Sprite):
 
+    def __init__(self, x, y):
+        super().__init__()
+        self.size = (100, 50)
+        self.image = pygame.image.load("RedRectangle.png")
+        self.rect = pygame.Rect((x, y), self.size)
+        self.x = x
+        self.y = y
+        self.color = RED
 
+blocks = pygame.sprite.Group()
 
+for i in range(10, 891, 110):
+    for j in range(25, 475, 55):
+        blocks.add(Brick(i,j))
 
 while True:
 
@@ -44,6 +49,8 @@ while True:
             sys.exit()
 
     DISPLAY_SURFACE.fill(BLUE)
+    for block in blocks:
+        DISPLAY_SURFACE.blit(block.image, block.rect)
 
     pygame.display.update()
     FramePerSec.tick(FPS)
